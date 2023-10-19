dcf7a3b3d75475826ac95fec80933c43aed3826f
Gregory Freire <gregory.freire@cuidar.me>
2023-10-19 15:32:54 -0300

    fix " Removido a lógica do modelo account "

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,34 +7,8 @@ class Account < ApplicationRecord
 
   validates :account_type, :current_balance, :customer_id, :bank_branch_id, presence: true
   
-  def depositar(transaction_value)
-    self.current_balance += transaction_value
-    save
-  end
-
-  def transferir(transaction_value, customer_id)
-    if self.current_balance >= transaction_value
-      self.current_balance -= transaction_value
-      Customer.depositar(transaction_value)
-      save
-      Customer.save
-    else
-      self.current_balance < transaction_value
-    end
-  end
-
-  def pagar(transaction_value)
-    if self.current_balance >= transaction_value
-      self.current_balance -= transaction_value
-      save
-    else
-      self.current_balance < transaction_value
-    end
-  end
-
   enum account_type: {
     checking_account: 0,
     savings_account: 1
   }
-  
 end
