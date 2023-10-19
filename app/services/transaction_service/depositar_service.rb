bc87885187936fb282873a5fa965d1180b1733f6
Gregory Freire <gregory.freire@cuidar.me>
2023-10-19 16:31:13 -0300

    fix "retirado o metodo call e arrumado a sintaxe"

--- app/services/transaction_service/depositar_service.rb
+++ app/services/transaction_service/depositar_service.rb
@@ -5,11 +5,6 @@ module TransactionService
       @bank_transaction = bank_transaction
     end
 
-    def call
-      depositar
-    end
-
-    private
       def depositar
         @account.current_balance += @bank_transaction.transaction_value
         @account.save 
