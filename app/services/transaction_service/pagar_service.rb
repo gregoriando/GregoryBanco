36b82f65e7d724df8ef369bbae8467fe1fde340d
Gregory Freire <gregory.freire@cuidar.me>
2023-10-24 19:09:38 -0300

    feat " refatoramento de lógica"

--- app/services/transaction_service/pagar_service.rb
+++ app/services/transaction_service/pagar_service.rb
@@ -1,18 +1,35 @@
 module TransactionService
   class PagarService 
-    def initialize(account, bank_transaction)
+    def initialize(account, pagamento)
       @account = account 
-      @bank_transaction = bank_transaction
+      @pagamento = pagamento
     end
 
 	  def pagar
-	    if @account.current_balance >= @bank_transaction.transaction_value
-				@account.current_balance -= @bank_transaction.transaction_value
+		ActiveRecord::Base.transaction do
+      pagando!
+		end
+	end
+
+  private
+		def pagando!
+	    if @account.current_balance >= @pagamento
+				@account.current_balance-= @pagamento
 				@account.save 
+        create_transaction(@account, 'deposito', @pagamento)
+
 			else 
 				raise "Saldo infusiciente para realizar o pagamento"
 			end
 		end
+
+    def create_transaction(account, transaction_type, pagamento)
+      BankTransaction.create(
+        account: account,
+        transaction_type: transaction_type,
+        transaction_value: pagamento
+      )
+    end
 	end
 end
 
